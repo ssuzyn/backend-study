202644111922038190ed8809803122a636086ba3
ssuzyn <asj7090@gmail.com>
2023-01-23 19:54:31 +0900

    read 구현

--- Week3/crud/views.py
+++ Week3/crud/views.py
@@ -5,11 +5,47 @@ import random
 첫번째 파라미터 인자는 요청과 관련된
 여러가지 정보가 들어오도록 약속된 객체를 전달
 """
+
+topics = [
+    {'id':1, 'title':'Routing', 'body':'Routing is ..'},
+    {'id':2, 'title':'View', 'body':'View is ..'},
+    {'id':3, 'title':'Model', 'body':'Model is ..'},
+]
+
+def HTMLTemplate(articleTag):
+    global topics #전역변수로 사용하겠다
+    ol =''
+    for topic in topics:
+        ol += f'<li><a href="/read/{topic["id"]}">{topic["title"]}</a></li>'
+    return f'''
+    <html>
+    <body>
+        <h1><a href="/">Django</a></h1>
+        <ul>
+            {ol}
+        </ul>
+        {articleTag}
+        <ul>
+            <li><a href="/create/">create</a></li>
+        </ul>
+    </body>
+    </html>
+    '''
+
 def index(request):
-    return HttpResponse('<h1>Random</h1>' + str(random.random()))
+    article = '''
+    <h2>Welcome</h2>
+    Hello, Django
+    '''
+    return HttpResponse(HTMLTemplate(article))
 
 def create(request):
     return HttpResponse('Create')
 
 def read(request, id):
-    return HttpResponse('Read' + id)
+    global topics
+    article = ''
+    for topic in topics:
+        if topic['id'] == int(id):
+            article = f'<h2>{topic["title"]}</h2>{topic["body"]}'
+    return HttpResponse(HTMLTemplate(article))
