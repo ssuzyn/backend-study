2495e5f8bda745f23dd7120cc51f623ccff2092c
ssuzyn <asj7090@gmail.com>
2023-01-30 16:07:56 +0900

    사용자 test data 생성후 반환 코드 구현

---
+++ Week4/myapp/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path, include
+from myapp import views
+
+urlpatterns = [
+    path('', views.main),
+    path('v1/testing/testingUser', views.testUser)
+]
--- Week4/myapp/views.py
+++ Week4/myapp/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.shortcuts import HttpResponse
+from django.http import JsonResponse
+from django.core import serializers
+from django.views.decorators.csrf import csrf_exempt
+from myapp.seed_models import testData
+from myapp.models import User
+import json
 
-# Create your views here.
+T = testData()
+
+def main(request):
+    return HttpResponse('''SQL Processing Page''')
+
+@csrf_exempt
+def testUser(request):
+    if request.method == 'POST':
+        try:
+            total = json.loads(request.body)['seed']
+            #user = T.handleUser(total)
+            user = User.objects.all()
+            user_list = serializers.serialize("json", user, fields=("email", "password"))
+            result = json.loads(user_list)
+            return JsonResponse(result, safe=False, status=200)
+        except Exception as ex:
+            print('error !!', ex)
+            return JsonResponse({"status" : "400"})
+            
